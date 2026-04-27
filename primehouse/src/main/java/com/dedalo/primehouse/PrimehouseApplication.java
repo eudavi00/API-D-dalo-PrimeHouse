6fc19b35236a3c5a1f131c9eeac6b27e24438c98
Davi <dmms0104@gmail.com>
2026-04-27 16:36:25 -0300

    criaçao do primehouse

---
+++ primehouse/src/main/java/com/dedalo/primehouse/PrimehouseApplication.java
@@ -0,0 +1,14 @@
+package com.dedalo.primehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimehouseApplication.class, args);
+		System.out.println("Saracuruna");
+	}
+
+}
---
+++ primehouse/src/test/java/com/dedalo/primehouse/PrimehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dedalo.primehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
